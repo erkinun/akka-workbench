c69fc0b8daa753f8b748ed35b4efe9495884bda5
lost_mohican <erkinun@gmail.com>
2018-05-20 13:52:05 +0100

    moved http logic into its own object

---
+++ src/main/scala/com/commutopia/HTTPServer.scala
@@ -0,0 +1,103 @@
+package com.commutopia
+
+import java.nio.file.Paths
+
+import akka.http.scaladsl.common.{EntityStreamingSupport, JsonEntityStreamingSupport}
+import akka.http.scaladsl.model.{ContentTypes, HttpEntity}
+import akka.http.scaladsl.server.Route
+import akka.http.scaladsl.server.Directives._
+import akka.stream.IOResult
+import akka.stream.scaladsl.{FileIO, Framing, Source}
+import akka.util.ByteString
+
+import scala.concurrent.{ExecutionContext, Future}
+import scala.util.{Random, Success}
+
+object HTTPServer extends JsonSupport  {
+
+  implicit val ec = ExecutionContext.global
+  
+  val csvFile = Paths.get("files/results.csv")
+  val fileSource: Source[ByteString, Future[IOResult]] = FileIO.fromPath(csvFile)
+  val lineSource: Source[CsvLine, Future[IOResult]] =
+    fileSource.via(Framing.delimiter(ByteString("\r\n"), maximumFrameLength = 100, allowTruncation = true))
+      .map(_.utf8String)
+      .map(line => CsvLine(line.split(", ").toList))
+
+  val numbers = Source.fromIterator(() =>
+    Iterator.continually(Random.nextInt()))
+
+  val futureExec = Future {
+    Random.nextInt()
+  }
+
+  def fileResource: Route = {
+
+    implicit val jsonStreamingSupport: JsonEntityStreamingSupport = EntityStreamingSupport.json()
+
+    path("file") {
+      get {
+        complete {
+          HttpEntity(
+            ContentTypes.`text/plain(UTF-8)`,
+            fileSource
+          )
+        }
+      }
+    } ~
+      path ("line") {
+        get {
+          complete {
+            lineSource
+          }
+        }
+      }
+  }
+
+  def futureResource(futureExec: Future[Int])(implicit ec: ExecutionContext): Route = {
+    path("future") {
+      get {
+        onComplete(futureExec) {
+          case Success(value) => complete(s"result is $value")
+        }
+      }
+    } ~
+      path("futureDirect") {
+        get {
+          import akka.http.scaladsl.marshalling.GenericMarshallers.futureMarshaller
+          complete(Future(Person("hede", 52)))
+        }
+      }
+  }
+
+  def personResource: Route = {
+    path("person") {
+      get {
+        complete {
+          Person("erkin", 32)
+        }
+      }
+    }
+  }
+
+  val route =
+    path("hello") {
+      get {
+        complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Say hello to akka-http</h1>"))
+      }
+    } ~
+      path("random") {
+        get {
+          complete(
+            HttpEntity(
+              ContentTypes.`text/plain(UTF-8)`,
+              // transform each number to a chunk of bytes
+              numbers.map(n => ByteString(s"$n\n"))
+            )
+          )
+        }
+      } ~
+      futureResource(futureExec) ~
+      personResource ~
+      fileResource
+}
--- src/main/scala/com/commutopia/WebServer.scala
+++ src/main/scala/com/commutopia/WebServer.scala
@@ -1,21 +1,11 @@
 package com.commutopia
 
-import java.nio.file.Paths
-
 import akka.actor.ActorSystem
-import akka.stream.scaladsl._
-import akka.util.ByteString
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.common.{EntityStreamingSupport, JsonEntityStreamingSupport}
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
-import akka.http.scaladsl.model._
-import akka.http.scaladsl.server.Directives._
-import akka.http.scaladsl.server.Route
-import akka.stream.{ActorMaterializer, IOResult}
+import akka.stream.ActorMaterializer
 import spray.json.DefaultJsonProtocol
 
-import scala.concurrent.{ExecutionContext, Future}
-import scala.util.{Random, Success}
 import scala.io.StdIn
 
 case class Person(name: String, age: Int)
@@ -35,68 +25,13 @@ object WebServer extends JsonSupport {
     // needed for the future flatMap/onComplete in the end
     implicit val executionContext = system.dispatcher
 
-    val numbers = Source.fromIterator(() =>
-      Iterator.continually(Random.nextInt()))
-
-    val futureExec = Future {
-      Random.nextInt()
-    }
-
-    val route =
-      path("hello") {
-        get {
-          complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Say hello to akka-http</h1>"))
-        }
-      } ~
-      path("random") {
-        get {
-          complete(
-            HttpEntity(
-              ContentTypes.`text/plain(UTF-8)`,
-              // transform each number to a chunk of bytes
-              numbers.map(n => ByteString(s"$n\n"))
-            )
-          )
-        }
-      } ~
-      futureResource(futureExec) ~
-      personResource() ~
-      HTTP.fileResource
-
-    val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
+    val bindingFuture = Http().bindAndHandle(HTTPServer.route, "localhost", 8080)
 
     println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
     StdIn.readLine() // let it run until user presses return
     bindingFuture
       .flatMap(_.unbind()) // trigger unbinding from the port
       .onComplete(_ => system.terminate()) // and shutdown when done
-  }
-
-  private def futureResource(futureExec: Future[Int])(implicit ec: ExecutionContext): Route = {
-    path("future") {
-      get {
-        onComplete(futureExec) {
-          case Success(value) => complete(s"result is $value")
-        }
-      }
-    } ~
-    path("futureDirect") {
-      get {
-        import akka.http.scaladsl.marshalling.GenericMarshallers.futureMarshaller
-        complete(Future(Person("hede", 52)))
-      }
-    }
-  }
-
-  private def personResource(): Route = {
-    
-    path("person") {
-      get {
-        complete {
-          Person("erkin", 32)
-        }
-      }
-    }
   }
 
 }
